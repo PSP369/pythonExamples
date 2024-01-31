594bbe08be86f14d45b68b761de4d5aa0dba4181
PSP369 <156859865+PSP369@users.noreply.github.com>
2024-01-30 20:18:59 -0500

    1/30
    Exmaples of python OOP

---
+++ PycharmProjects/pythonProject1/main.py
@@ -0,0 +1,9 @@
+# Function to add two numbers
+def add_two_numbers():
+    num1 = float(input("Enter first number:"))
+    num2 = float(input("Enter first number:"))
+    sum = num1 + num2
+    print("The sum of the number is:", sum)
+
+
+add_two_numbers()
